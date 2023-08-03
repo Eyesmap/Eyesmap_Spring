511ed068fab68f2968adff970d9c23e660974ba1
hil679 <hil679922@gmail.com>
2023-08-03 13:19:22 +0900

    first commit

---
+++ src/main/java/com/spring/eyesmap/EyesmapApplication.java
@@ -0,0 +1,13 @@
+package com.spring.eyesmap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EyesmapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EyesmapApplication.class, args);
+	}
+
+}
