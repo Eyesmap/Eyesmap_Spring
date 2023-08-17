0fd8d894f4e1d2f673e06880f23487cacacf60f4
naingyu <sktmdgus1212@naver.com>
2023-08-17 17:42:28 +0900

    .

--- src/main/java/com/spring/eyesmap/domain/account/controller/LoginController.java
+++ src/main/java/com/spring/eyesmap/domain/account/controller/LoginController.java
@@ -4,13 +4,13 @@ import com.spring.eyesmap.domain.account.dto.LoginResponseDto;
 import com.spring.eyesmap.domain.account.dto.LogoutResponseDto;
 import com.spring.eyesmap.domain.account.service.LoginService;
 import com.spring.eyesmap.global.dto.ResponseDto;
-import com.spring.eyesmap.global.exception.LoginFailedException;
-import jakarta.servlet.http.HttpSession;
-import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
