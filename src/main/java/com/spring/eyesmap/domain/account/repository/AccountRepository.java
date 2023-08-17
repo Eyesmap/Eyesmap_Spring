9f69b6a2640d1b963f37ff5052685c886b35037e
naingyu <sktmdgus1212@naver.com>
2023-08-17 17:36:50 +0900

    divide domain and repository (account entity)

--- src/main/java/com/spring/eyesmap/domain/account/repository/AccountRepository.java
+++ src/main/java/com/spring/eyesmap/domain/account/repository/AccountRepository.java
@@ -1,5 +1,6 @@
 package com.spring.eyesmap.domain.account.repository;
 
+import com.spring.eyesmap.domain.account.domain.Account;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
