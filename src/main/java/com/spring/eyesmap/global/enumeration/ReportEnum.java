2ad978e8fa78034cbfd25dd92c32cdb97cd530c0
hil679 <hil679922@gmail.com>
2023-08-21 12:55:34 +0900

    change: report sort

--- src/main/java/com/spring/eyesmap/global/enumeration/ReportEnum.java
+++ src/main/java/com/spring/eyesmap/global/enumeration/ReportEnum.java
@@ -10,7 +10,7 @@ public class ReportEnum {
         DAMAGE, RESTORE, COMPLETE
     }
     public enum Sort{
-        DOTTED_BLOCK, LINEAR_BLOCK, ACOUSTIC_SIGNAL, VOICE_GUIDE, BRAILLE_SIGN, TACTILE_MAP
+        DOTTED_BLOCK, ACOUSTIC_GUIDENCE_SYSTEM, BRAILLE_INFO_BOARD
     }
     public enum DamagedStatus{
         NORMAL, BAD, SEVERE
