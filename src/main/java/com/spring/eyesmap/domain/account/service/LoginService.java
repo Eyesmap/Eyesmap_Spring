30fc7cc0c7a52b01536b19879abbd42c27dfade8
hil679 <121854089+hil679@users.noreply.github.com>
2023-09-06 13:18:18 +0900

    Update LoginService.java based on review

--- src/main/java/com/spring/eyesmap/domain/account/service/LoginService.java
+++ src/main/java/com/spring/eyesmap/domain/account/service/LoginService.java
@@ -93,7 +93,7 @@ public class LoginService {
                 .userId(kakao_id)
                 .nickname(nickName)
                 .role(Role.ROLE_USER)
-                .profileImageUrl(bucket +
+                .profileImageUrl("https://" + bucket +
                         ".s3." +
                         region +
                         ".amazonaws.com/account/profile/image/" +
